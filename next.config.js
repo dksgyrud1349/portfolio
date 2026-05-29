1bfbc13e54148bf2537ffc8cace07073df418fdd
안효경 <dksgyrud1349@naver.com>
2026-05-29 13:21:08 +0900

    Add diagram

--- next.config.js
+++ next.config.js
@@ -1,24 +1,17 @@
-/* eslint-disable */
-const withImages = require('next-images');
 const { homepage } = require('./package.json');
 
-const { NODE_ENV } = process.env;
+const basePath =
+  process.env.NODE_ENV === 'production' && homepage
+    ? new URL(homepage).pathname
+    : '';
 
-module.exports = withImages({
-  assetPrefix: (() => {
-    if (NODE_ENV === 'production' && homepage) {
-      try {
-        const { pathname } = new URL(homepage);
-        // GitHub Pages 서브 경로 적용
-        return pathname !== '/' ? pathname : '';
-      } catch {
-        console.warn('> Invalid homepage URL in package.json, assetPrefix not applied');
-        return '';
-      }
-    }
-    return '';
-  })(),
+module.exports = {
+  output: 'export',
+
+  basePath,
+  assetPrefix: basePath,
+
   images: {
-    unoptimized: true, // GitHub Pages에서는 이미지 최적화 비활성화
+    unoptimized: true,
   },
-});
+};
